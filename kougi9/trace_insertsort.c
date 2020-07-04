154faa61c887aea80abe4508b92527de73b27b55
shoumoji <kosyu79@gmail.com>
2020-07-04 21:24:11 +0900

    トレース表出力を追加

---
+++ kougi9/trace_insertsort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void outArrayTrace();
+
+//ソートする配列。個数、値ともにランダム
+int A[] = {5, 2, 10, 3};
+
+int count = 0;
+unsigned int arraySize = sizeof(A) / sizeof(A[0]);
+
+int main(void) {
+  int buf;
+
+  // iは挿入する配列のための変数
+  // jは比較する配列のための変数
+  int i, j, k;
+
+  outArrayTrace();
+
+  // A[0]は挿入しないので、A[1]から挿入の可否を調べる
+  for (i = 1; i < arraySize; i++) {
+    // A[i]より左の数値と比較していく
+    for (j = 0; j < i; j++) {
+      if (A[i] < A[j]) {
+        buf = A[i];
+        // A[j]からA[i]までの配列を右に１つ全てずらす
+        for (k = i; k > j; k--) {
+          A[k] = A[k - 1];
+        }
+        // ずらし終わったら、元のA[i]の値をA[j]に挿入
+        A[j] = buf;
+
+        // トレースを出力
+        count++;
+        outArrayTrace();
+
+        break;  //挿入したら、A[i]の挿入場所はそれ以降調べない
+      }
+    }
+  }
+}
+
+void outArrayTrace() {
+  if (count == 0) {
+    printf("与えられた配列: A[] = {%d", A[0]);
+  } else {
+    printf("%d回目: A[] = {%d", count, A[0]);
+  }
+  for (int l = 1; l < arraySize; l++) {
+    printf(", %d", A[l]);
+  }
+  printf("}\n");
+}
