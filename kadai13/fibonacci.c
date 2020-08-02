d4d850cbb1da1089666cabfcde17ebb1a999dd43
shoumoji <kosyu79@gmail.com>
2020-08-02 21:38:22 +0900

    大きな桁に対応

--- kadai13/fibonacci.c
+++ kadai13/fibonacci.c
@@ -1,13 +1,15 @@
+#define uint64t __uint64_t
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci(const int n) {
+uint64t fibonacci(const int n) {
   if (n == 0) {
     return 0;
   } else if (n == 1) {
     return 1;
   } else {
-    int fib = 0;
+    uint64t fib = 0;
     fib += fibonacci(n - 1) + fibonacci(n - 2);
     return fib;
   }
@@ -18,6 +20,6 @@ int main(const int argc, const char *argv[]) {
     exit(1);
   }
   int inputNum = atoi(argv[1]);
-  int result = fibonacci(inputNum - 1) + fibonacci(inputNum - 2);
-  printf("%dのフィボナッチ数は%dです", inputNum, result);
+  uint64t result = fibonacci(inputNum - 1) + fibonacci(inputNum - 2);
+  printf("%dのフィボナッチ数は%luです", inputNum, result);
 }
