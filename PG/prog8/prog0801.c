f49e45cc4f296d6d941f8762dcdcf00a3683e90a
shoumoji <kosyu79@gmail.com>
2020-11-15 21:49:25 +0900

    とりあえず動くように

--- PG/prog8/prog0801.c
+++ PG/prog8/prog0801.c
@@ -4,33 +4,33 @@
 #define ODD(n) (n % 2 == 1 ? n : 0)
 
 int main(void) {
-    int num1, num2, num3, num4, ans;
-    int num, i, sum = 0;
+  int num1, num2, num3, num4, ans;
+  int num, i, sum = 0;
 
-    printf("1番目の整数を入力してください\n");
-    scanf("%d", &num1);
-    printf("2番目の整数を入力してください\n");
-    scanf("%d", &num2);
-    printf("3番目の整数を入力してください\n");
-    scanf("%d", &num3);
-    printf("4番目の整数を入力してください\n");
-    scanf("%d", &num4);
+  printf("1番目の整数を入力してください\n");
+  scanf("%d", &num1);
+  printf("2番目の整数を入力してください\n");
+  scanf("%d", &num2);
+  printf("3番目の整数を入力してください\n");
+  scanf("%d", &num3);
+  printf("4番目の整数を入力してください\n");
+  scanf("%d", &num4);
 
-    ans = MAX(MAX(num1, num2), MAX(num3, num4));
-    printf("最大値は%dです。\n\n", ans);
+  ans = MAX(MAX(num1, num2), MAX(num3, num4));
+  printf("最大値は%dです。\n\n", ans);
 
-    while (1) {
-      printf("いくつまで奇数の和を計算しますか。(正の整数のみ)\n");
-      scanf("%d", &num);
-      if (num > 0) {
-        break;
-      }
+  while (1) {
+    printf("いくつまで奇数の和を計算しますか。(正の整数のみ)\n");
+    scanf("%d", &num);
+    if (num > 0) {
+      break;
     }
+  }
 
-    for (i = 0; i <= num; i++) {
-      sum += ODD(i);
-    }
+  for (i = 0; i <= num; i++) {
+    sum += ODD(i);
+  }
 
-    printf("%dまでの奇数の和は%dです。", num, sum);
-    return 0;
+  printf("%dまでの奇数の和は%dです。", num, sum);
+  return 0;
 }
---
+++ PG/prog8/prog0803_2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void output_print(double l, double w, double h);
+double calc_surface(double l, double w, double h);
+double calc_volume(double l, double w, double h);
+
+int main(void) {
+  double length, width, height, surface, volume;
+
+  printf("長さ(cm)を入力してください。¥n");
+  scanf("%lf", &length);
+  printf("幅(cm)を入力してください。¥n");
+  scanf("%lf", &width);
+  printf("高さ(cm)を入力してください。¥n");
+  scanf("%lf", &height);
+
+  output_print(length, width, height);
+  printf("表面積は%6.2fcm2です。\n", calc_surface(length, width, height));
+
+  output_print(length, width, height);
+  printf("体積は%6.2fcm3です。\n", calc_volume(length, width, height));
+
+  return 0;
+}
+
+void output_print(double l, double w, double h) {
+  printf("長さ: %6.2fcm, 幅: %6.2fcm, 高さ: %6.2fcm の", l, w, h);
+}
+
+double calc_surface(double l, double w, double h) {
+  double surface;
+  surface = (l * w + w * h + h * l) * 2.0;
+  return surface;
+}
+
+double calc_volume(double l, double w, double h) {
+  double volume;
+  volume = l * w * h;
+  return volume;
+}
