cb910934c9208c9bd43b5cd020326c83590c26d6
shoumoji <kosyu79@gmail.com>
2020-11-15 21:15:57 +0900

    うまくformatできない

--- PG/prog8/prog0802.c
+++ PG/prog8/prog0802.c
@@ -3,45 +3,45 @@
 int count = 10;
 
 int count_func(void) {
-  count++;
-  printf("count_func:%d\n", count);
+    count++;
+    printf("count_func:%d\n", count);
 
-  return 0;
+    return 0;
 }
 
 void max_local(int n) {
-  int max = 0;
+    int max = 0;
 
-  if (n > max) {
-    max = n;
-  }
-  printf("max_local : max=%d\n", max);
+    if (n > max) {
+      max = n;
+    }
+    printf("max_local : max=%d\n", max);
 }
 
 void max_static(int n) {
-  static int max = 0;
+    static int max = 0;
 
-  if (n > max) {
-    max = n;
-  }
-  printf("max_static : max=%d\n", max);
+    if (n > max) {
+      max = n;
+    }
+    printf("max_static : max=%d\n", max);
 }
 
 int main(void) {
-  int count = 10;
+    int count = 10;
 
-  count++;
-  printf("main:%d\n", count);
-  count_func();
-  count++;
-  printf("main:%d\n", count);
+    count++;
+    printf("main:%d\n", count);
+    count_func();
+    count++;
+    printf("main:%d\n", count);
 
-  max_local(30);
-  max_local(60);
-  max_local(50);
-  max_local(30);
-  max_local(60);
-  max_local(50);
+    max_local(30);
+    max_local(60);
+    max_local(50);
+    max_local(30);
+    max_local(60);
+    max_local(50);
 
-  return 0;
+    return 0;
 }
--- PG/prog8/prog0803.c
+++ PG/prog8/prog0803.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-  double length, width, height, surface, volume;
+       double length, width, height, surface, volume;
 
-  printf("長さ(cm)を入力してください。¥n");
-  scanf("%lf", &length);
-  printf("幅(cm)を入力してください。¥n");
-  scanf("%lf", &width);
-  printf("高さ(cm)を入力してください。¥n");
-  scanf("%lf", &height);
+       printf("長さ(cm)を入力してください。¥n");
+       scanf("%lf", &length);
+       printf("幅(cm)を入力してください。¥n");
+       scanf("%lf", &width);
+       printf("高さ(cm)を入力してください。¥n");
+       scanf("%lf", &height);
 
-  surface = (length * width + width * height + height * length) * 2.0;
-  volume = length * width * height;
+       surface = (length * width + width * height + height * length) * 2.0;
+       volume = length * width * height;
 
-  printf("長さ：%6.2fcm、幅：%6.2fcm、高さ：%6.2fcm の直方体の表面積は%6.2fcm2 "
-         "です。¥n",
-         length, width, height, surface);
-  printf("長さ：%6.2fcm、幅：%6.2fcm、高さ：%6.2fcm の直方体の体積は%6.2fcm3 "
-         "です。¥n",
-         length, width, height, volume);
+       printf("長さ：%6.2fcm、幅：%6.2fcm、高さ：%6.2fcm の直方体の表面積は%6.2fcm2 "
+              "です。¥n",
+              length, width, height, surface);
+       printf("長さ：%6.2fcm、幅：%6.2fcm、高さ：%6.2fcm の直方体の体積は%6.2fcm3 "
+              "です。¥n",
+              length, width, height, volume);
 
-  return 0;
+       return 0;
 }
